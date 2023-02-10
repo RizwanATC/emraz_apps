aaf679ed1eba6f2110236834ec86440444a213f7
Rizwan <rizwanmatnawi00@gmail.com>
2023-02-10 17:56:51 +0800

    bottomsheetlayout.xml add

--- app/src/main/java/com/example/emraz_apps/Payment_selection.java
+++ app/src/main/java/com/example/emraz_apps/Payment_selection.java
@@ -1,37 +1,75 @@
 package com.example.emraz_apps;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.animation.LinearInterpolator;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Payment_selection extends AppCompatActivity {
+
+    Button  proceedPayment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.payment_select);
+        proceedPayment = findViewById(R.id.paymentPro);
+        proceedPayment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog();
+            }
+        });
 
-        //intit views
-        LinearLayout ll = (LinearLayout) findViewById(R.id.firstLayout);
-        RadioGroup radioGroup = findViewById(R.id.radioGroup);
 
-        //create the listener for radio group
+    }
+
+    private void showDialog() {
+
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.bottomsheetlayout);
+
+        Button button;
+        RadioButton radioButton1;
+        RadioGroup radioGroup;
+        radioGroup=(RadioGroup)dialog.findViewById(R.id.radioGroup);
+        radioButton1 = (RadioButton) dialog.findViewById(R.id.radio_button_choice1);
+        LinearLayout ll1 = (LinearLayout) dialog.findViewById(R.id.firstLayout);
+
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                //check if the choice-1 is selected
-                if (radioGroup.getCheckedRadioButtonId() == R.id.radio_button_choice1) {
-                    //show image wen select choice-1
-                    ll.setVisibility(View.VISIBLE);
-                } else {
-                    //hide image when choice-1 not selected
-                    ll.setVisibility(View.GONE);
+                if(radioButton1.isChecked()){
+                    ll1.setVisibility(View.VISIBLE);
+                }else{
+                    ll1.setVisibility(View.GONE);
                 }
+
+
             }
         });
-    }
 
+
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialoAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
+
+
+    }
 }
